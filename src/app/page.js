1fbcd2e7c563fd323f996240eb65e9b764d3a89a
Sonia Ruiz <yune.vk@gmail.com>
2023-11-03 11:04:06 +0100

    🌐 i18n basic setup

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
 import Button from "@mui/material/Button"
 
+import useTranslations from "@/i18n/useTranslations"
+
 export default function Home() {
+  const { t } = useTranslations()
+
   return (
     <main>
-      <Button variant="contained">Hello world</Button>
+      <Button variant="contained">{t("title:site")}</Button>
     </main>
   )
 }
---
+++ src/i18n/useTranslations.js
@@ -0,0 +1,33 @@
+import { useCallback } from "react"
+
+import regionlessEnglish from "./messages/en.json"
+
+const useTranslations = () => {
+  const formatMessage = useCallback((id, values) => {
+    let template = regionlessEnglish[id]
+
+    if (!template) {
+      console.warn(`⚠️ Translation for '${id}' not found.`)
+      template = id
+    }
+
+    // Replace placeholders with values
+    if (values) {
+      Object.keys(values).forEach((key) => {
+        const regex = new RegExp(`{${key}}`, "g")
+
+        if (!regex.test(template)) {
+          console.warn(`⚠️ Placeholder '{${key}}' not found for '${id}'.`) // Warn if placeholder missing.
+        }
+
+        template = template.replace(regex, values[key])
+      })
+    }
+
+    return template
+  }, [])
+
+  return { t: formatMessage }
+}
+
+export default useTranslations
